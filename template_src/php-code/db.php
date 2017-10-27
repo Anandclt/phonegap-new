a47f2153355f3cacb0998c9351dda966e86b006e
Anandclt <anandbe13@gmail.com>
2017-10-27 18:42:16 +0530

    Add files via upload

--- template_src/php-code/db.php
+++ template_src/php-code/db.php
@@ -1,4 +1,5 @@
 <?php
  header("Access-Control-Allow-Origin: *");
- $con = mysqli_connect("localhost","root","","phonegap_demo") or die ("could not connect database");
+ //$con = mysqli_connect("localhost","root","","phonegap_demo") or die ("could not connect database");
+  $con = mysqli_connect("localhost","connecti_cmsuser","~4ccJrM60sdq","connecti_global_hospitality_cms") or die ("could not connect database");
 ?>
